5a804b97e079fd2551427e3fa5521592833b6fa7
Madjar Evan <evanmadjar@gmail.com>
2024-02-07 14:22:06 +0200

    refactor: Update url creation

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -5,10 +5,12 @@ from typing import NoReturn
 from fastapi import (
     FastAPI,
     Depends,
+    Request,
     HTTPException,
 )
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
-from . import schemas, models
+from . import schemas, models, crud
 from .database import SessionLocal, engine
 
 
@@ -28,28 +30,37 @@ def raise_bad_request(message: str) -> NoReturn:
     raise HTTPException(status_code=400, detail=message)
 
 
+def raise_not_found(request: Request) -> NoReturn:
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
 @app.get('/')
 def read_root():
     return 'Welcome to the URL shortener API'
 
 
+@app.get('/{url_key}')
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db),
+    ):
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        return RedirectResponse(db_url.target_url)
+    return raise_not_found(request)
+
+
 @app.post('/url', response_model=schemas.URLInfo)
-def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+def create_url(
+        url: schemas.URLBase,
+        db: Session = Depends(get_db)
+    ):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = ''.join(secrets.choice(chars) for _ in range(8))
-    db_url: models.URL = models.URL(
-        target_url=url.target_url,
-        key=key,
-        secret_key=secret_key,
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url: models.URL = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
     
     return db_url
